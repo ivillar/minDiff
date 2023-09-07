ad69801ffc1b1b4b02f61761a4a7391e552df13d
Ivan Villa-Renteria <villarenteriai@gmail.com>
2023-09-06 23:40:33 -0700

    Add TODOs and fix unbroadcast_gradient typo

--- mindiff.py
+++ mindiff.py
@@ -1,13 +1,14 @@
 import math
 import numpy as np
 
+
 class Tensor:
-    def __init__(self, data, _children = (), _op = ''):
+    def __init__(self, data, _children=(), _op=""):
         self.data = data if isinstance(data, np.ndarray) else np.array(data)
         self._prev = set(_children)
         self._op = _op
         self.grad = 0.0
-        self._backward = lambda : None
+        self._backward = lambda: None
         self.label = None
 
     def backward(self):
@@ -31,10 +32,10 @@ class Tensor:
 
     def __repr__(self):
         return f"Tensor(data={self.data})"
-    
+
     def __add__(self, other):
         other = self._other_check(other)
-        out = Tensor(self.data + other.data, (self, other), '+')
+        out = Tensor(self.data + other.data, (self, other), "+")
 
         def backward():
             self_unbroadcast_outgrad = self.unbroadcast_gradient(self.data, out.grad)
@@ -44,25 +45,28 @@ class Tensor:
 
         out._backward = backward
         return out
-    
+
     def __mul__(self, other):
+        """
+        TODO: add unbroadcast check in backward
+        """
         other = self._other_check(other)
-        out = Tensor(self.data * other.data, (self, other), '*')
-        
+        out = Tensor(self.data * other.data, (self, other), "*")
+
         def backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
-        
+
         out._backward = backward
         return out
 
     def __rmul__(self, other):
         return self * other
-    
+
     def __matmul__(self, other):
         other._other_check(other)
-        out = Tensor(self.data @ other.data, (self, other), '@')
-        
+        out = Tensor(self.data @ other.data, (self, other), "@")
+
         def backward():
             self.grad += out.grad @ other.data.T
             other.grad += self.data.T @ out.grad
@@ -72,7 +76,7 @@ class Tensor:
 
     def __pow__(self, other):
         other = self._other_check(other)
-        out = Tensor(self.data ** other.data, (self, other), '**')
+        out = Tensor(self.data**other.data, (self, other), "**")
 
         def backward():
             self.grad += (other.data * self.data ** (other.data - 1)) * out.grad
@@ -80,10 +84,10 @@ class Tensor:
 
         out._backward = backward
         return out
-    
+
     def __rpow__(self, other):
         other = self._other_check(other)
-        out = Tensor(other.data ** self.data, (self, other), '**')
+        out = Tensor(other.data**self.data, (self, other), "**")
 
         def backward():
             self.grad += out.data * math.log(other.data) * out.grad
@@ -94,44 +98,54 @@ class Tensor:
         return out
 
     def __truediv__(self, other):
-        return self * (other ** -1)
+        return self * (other**-1)
 
     def __rtruediv__(self, other):
-        return other * (self ** - 1)
+        return other * (self**-1)
 
     def __neg__(self):
-        out = Tensor(-self.data, (self,), '-')
-        
+        out = Tensor(-self.data, (self,), "-")
+
         def backward():
-            self.grad += - out.grad
+            self.grad += -out.grad
 
         out._backward = backward
         return out
 
     def __sub__(self, other):
-        return self + (- other)
+        """
+        TODO: add __rsub__ method
+        """
+        return self + (-other)
 
     def _other_check(self, other):
+        """
+        Checks to see whether other is an instance of Tensor. If it isn't it
+        converts it to one.
+        """
         other = other if isinstance(other, Tensor) else Tensor(other)
         return other
-    
+
     def sum(self, axis=None):
         """
         TODO: Add backward support for different dimensions of summation.
         Backward is only guaranteed to work for summing last axis of 2D array.
         """
-        out = Tensor(np.sum(self.data, axis=axis), (self,), 'sum')
+        out = Tensor(np.sum(self.data, axis=axis), (self,), "sum")
 
         def backward():
             m = self.data.shape[0]
             # distribute global gradient to m
-            self.grad += np.tile(np.expand_dims(out.grad, axis = 0), (m,1))
+            self.grad += np.tile(np.expand_dims(out.grad, axis=0), (m, 1))
 
         out._backward = backward
         return out
 
-
     def unbroadcast_gradient(self, child, global_gradient):
+        """
+        TODO: Review code and ensure that this *always* works, no matter how array is
+        broadcast
+        """
         correct_global_gradient = global_gradient
 
         if child.shape != global_gradient.shape:
@@ -139,10 +153,13 @@ class Tensor:
             if dimensions_diff != 0:
                 summation_dims = tuple(range(dimensions_diff))
                 correct_global_gradient = np.sum(global_gradient, axis=summation_dims)
-    
-                originally_ones = tuple([axis  for axis, size in enumerate(child.shape) if size == 1])
+
+                originally_ones = tuple(
+                    [axis for axis, size in enumerate(child.shape) if size == 1]
+                )
                 if len(originally_ones) != 0:
-                    correct_global_gradient = np.sum(correct_global_gradient, axis=axis, keepdims=True)
-    
+                    correct_global_gradient = np.sum(
+                        correct_global_gradient, axis=originally_ones, keepdims=True
+                    )
+
         return correct_global_gradient
-    
