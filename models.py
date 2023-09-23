0ad84241fdd856a5fb4a52db708812a243a5c3e3
Ivan Villa-Renteria <villarenteriai@gmail.com>
2023-09-23 03:29:57 -0700

    Create new MLP based on new softmax and CELoss module

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from mindiff import nn
 
 
-class MLPClassifier(nn.Module):
+class OldMLPClassifier(nn.Module):
     def __init__(self, layer_size_list):
         super().__init__()
         self.layers = []
@@ -14,6 +14,27 @@ class MLPClassifier(nn.Module):
         new_dims = layer_size_list[-1]
         self.layers.append(nn.Linear(old_dims, new_dims))
         self.layers.append(nn.Softmax())
+        self.layers = nn.ModuleList(self.layers)
+
+    def forward(self, X):
+        x = X
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+class NewMLPClassifier(nn.Module):
+    def __init__(self, layer_size_list):
+        super().__init__()
+        self.layers = []
+        for i in range(len(layer_size_list) - 2):
+            old_dims = layer_size_list[i]
+            new_dims = layer_size_list[i + 1]
+            self.layers.append(nn.Linear(old_dims, new_dims))
+            self.layers.append(nn.ReLU())
+        old_dims = layer_size_list[-2]
+        new_dims = layer_size_list[-1]
+        self.layers.append(nn.Linear(old_dims, new_dims))
         self.layers = nn.ModuleList(self.layers)
 
     def forward(self, X):
